0a651f590aab54c324d69ca0e426dfe967886754
Talyson Moreira <talyson.penha@gmail.com>
2020-11-25 23:36:43 -0300

    Initial Commit

---
+++ web/src/pages/CreateOrphanage.tsx
@@ -0,0 +1,169 @@
+import React, { FormEvent, useState } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+
+import { FiPlus } from "react-icons/fi";
+
+import Sidebar from "../components/Sidebar";
+import mapIcon from "../utils/mapIcon";
+
+import '../styles/pages/create_orphanage.css';
+
+export default function OrphanagesMap() {
+
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpeningHours] = useState('');
+  const [open_on_weekends, setOpenOnWeekends] = useState(true);
+  
+  function AddMarkerToClick() {
+
+    useMapEvents({
+      click(e) {
+        const { lat, lng } = e.latlng;
+
+        setPosition({
+          latitude: lat,
+          longitude: lng,
+        })
+      },
+    })
+
+    return (
+      <>
+        {position.latitude !== 0 && (
+          <Marker
+            interactive={false}
+            icon={mapIcon}
+            position={[
+              position.latitude,
+              position.longitude
+            ]}
+          />
+        )}
+      </>
+    )
+  }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { latitude, longitude } = position;
+  
+    console.log({
+      name,
+      about,
+      latitude,
+      longitude,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    })
+  }
+
+  return (
+    <div id="page-create-orphanage">
+      <Sidebar />
+
+      <main>
+        <form onSubmit={handleSubmit} className="create-orphanage-form">
+          <fieldset>
+            <legend>Dados</legend>
+
+            <MapContainer
+              center={[-2.5231534, -44.2394444]}
+              style={{ width: '100%', height: 280 }}
+              zoom={15}
+            >
+
+              <TileLayer
+                url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+
+              <AddMarkerToClick />
+            </MapContainer>
+
+            <div className="input-block">
+              <label htmlFor="name">Nome</label>
+              <input 
+                id="name" 
+                value={name} 
+                onChange={event => setName(event.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
+              <textarea 
+                id="name" 
+                maxLength={300}
+                value={about} 
+                onChange={event => setAbout(event.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="images">Fotos</label>
+              <div className="uploaded-image">
+              </div>
+
+              <button type="button" className="new-image">
+                <FiPlus size={24} color="#15b6d6" />
+              </button>
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>Visitação</legend>
+
+            <div className="input-block">
+              <label htmlFor="instructions">Instruções</label>
+              <textarea 
+                id="instructions"
+                value={instructions} 
+                onChange={event => setInstructions(event.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="opening_hours">Horário de Funcionamento</label>
+              <input 
+                id="opening_hours"
+                value={opening_hours} 
+                onChange={event => setOpeningHours(event.target.value)} 
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+
+              <div className="button-select">
+                <button 
+                  type="button" 
+                  className={open_on_weekends ? 'active' : ''}
+                  onClick={() => setOpenOnWeekends(true)}
+                >
+                  Sim
+                </button>
+                <button 
+                  type="button"
+                  className={!open_on_weekends ? 'active' : ''}
+                  onClick={() => setOpenOnWeekends(false)}
+                >
+                  Não
+                </button>
+              </div>
+            </div>
+          </fieldset>
+
+          <button className="confirm-button" type="submit">
+            Confirmar
+                </button>
+        </form>
+      </main>
+    </div>
+  );
+}
+
+// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
