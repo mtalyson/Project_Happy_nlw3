d2274c2d86c328750d6740dfceb4917bf4de9104
Talyson Moreira <talyson.penha@gmail.com>
2021-01-18 13:59:50 -0300

    First route implemented & refactored map page

---
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import mapMarker from '../images/map-marker.png';
+import { Feather } from '@expo/vector-icons';
+
+export default function OrphanagesMap() {
+   return(
+      <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -2.5213971,
+          longitude: -44.2400215,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.015,
+        }} 
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.8,
+            y: 0.85,
+          }}
+          coordinate={{
+            latitude: -2.5213971,
+            longitude: -44.2400215,
+          }}
+        >
+          <Callout tooltip onPress={() =>{
+            alert('teste')
+          }}>
+            <View style={styles.calloutContainer}>
+             <Text style={styles.calloutText}>Lar das Meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+          <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+
+          <TouchableOpacity style={styles.createOrphanageButton} onPress={() =>{}}>
+            <Feather name="plus" size={20} color="#FFF" ></Feather>
+          </TouchableOpacity>
+      </View>
+    </View>
+   );
+}
+
+const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+   },
+ 
+   map: {
+     width: Dimensions.get('window').width,
+     height: Dimensions.get('window').height,
+   },
+ 
+   calloutContainer: {
+     width: 160,
+     height: 46,
+     paddingHorizontal: 16,
+     backgroundColor: 'rgba(255, 255, 255, 0.8)',
+     borderRadius: 16,
+     justifyContent: 'center',
+   },
+ 
+   calloutText: {
+     color: '#0089a5',
+     fontSize: 14,
+     fontFamily: 'Nunito_700Bold',
+   },
+ 
+   footer: {
+     position: 'absolute',
+     left: 24,
+     right: 24,
+     bottom: 32,
+ 
+     backgroundColor: '#FFF',
+     borderRadius: 20,
+     height: 56,
+     paddingLeft: 24,
+ 
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+     alignItems: 'center',
+ 
+     elevation: 3,
+   },
+ 
+   footerText: {
+     fontFamily: 'Nunito_700Bold',
+     color: '#8fa7b3'
+   },
+ 
+   createOrphanageButton: {
+     width: 56,
+     height: 56,
+     backgroundColor: '#15c3d6',
+     borderRadius: 20,
+ 
+     justifyContent: 'center',
+     alignItems: 'center'
+   },
+ });
