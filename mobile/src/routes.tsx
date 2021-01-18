13fc032ebc9208f585087d301cddc8e5af69aaa5
Talyson Moreira <talyson.penha@gmail.com>
2021-01-18 15:25:11 -0300

    Registration flow & header implemented

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -6,12 +6,19 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
+
 
 export default function Routes(){
    return(
       <NavigationContainer>
          <Navigator screenOptions={{
-            headerShown: false
+            headerShown: false,
+            cardStyle: {
+               backgroundColor: '#f2f3f5'
+            }
          }}>
             <Screen 
                name="OrphanagesMap" 
@@ -21,6 +28,30 @@ export default function Routes(){
             <Screen 
                name="OrphanageDetails"
                component={OrphanageDetails}
+               options={{
+                  headerShown: true,
+                  header: () => <Header 
+                  showCancel={false}
+                  title="Orfanato" />
+               }}
+            />
+
+            <Screen 
+               name="SelectMapPosition"
+               component={SelectMapPosition}
+               options={{
+                  headerShown: true,
+                  header: () => <Header  title="Selecione no Mapa" />
+               }}
+            />
+
+            <Screen 
+               name="OrphanageData"
+               component={OrphanageData}
+               options={{
+                  headerShown: true,
+                  header: () => <Header title="Informe os Dados" />
+               }}
             />
 
          </Navigator>
